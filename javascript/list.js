c10e1409845de5361c3f666a40f4c9abe911a4f1
ecreeed <hall.ethan.au@gmail.com>
2026-05-17 14:15:20 +1000

    listing page

---
+++ javascript/list.js
@@ -0,0 +1,12 @@
+
+const search = document.getElementById('search');
+
+function fillSearch(value) {
+    search.value = value;
+    applySearch();
+    return false;
+}
+
+function applySearch() {
+
+}
